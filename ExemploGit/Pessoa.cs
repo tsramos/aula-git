c561b319cf57dffaab45b6ae9237c1477e357310
Thiago Ramos <thiago.s.ramos@outlook.com>
2020-06-02 22:16:50 -0300

    Criada classe pessoa

---
+++ ExemploGit/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExemploGit
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public Pessoa(string nome, DateTime dataNascimento)
+        {
+            Nome = nome;
+            DataNascimento = dataNascimento;
+        }
+    }
+}
