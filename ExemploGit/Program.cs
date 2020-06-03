487c658708a6b63a80764b211c0d827bff6fda68
Davi Ruhan de Oliveira <davi_ruhan@hotmail.com>
2020-06-02 23:24:15 -0300

    Testando o git pull

--- ExemploGit/Program.cs
+++ ExemploGit/Program.cs
@@ -8,6 +8,7 @@ namespace ExemploGit
         {
             Pessoa pessoa = new Pessoa("Pessoa", new DateTime(2000,08,01));
             Console.WriteLine("Terminei essa merda com sucesso acho que eles entenderam ,...");
+            Console.WriteLine("Mensagem qualquer ai pra nois");
         }
     }
 }
